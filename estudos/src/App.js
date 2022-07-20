e028602c3f5ff2745c380ee4711dec8b611fff73
Igor Bitencourt <bitencourt.igor@hotmail.com>
2022-07-20 09:45:13 -0300

    exercicio react checkbox

--- estudos/src/App.js
+++ estudos/src/App.js
@@ -1,11 +1,42 @@
 import React from "react";
-import Input from "./Input";
+
+const coresArray = ["azul", "roxo", "laranja", "verde", "vermelho", "cinza"];
 
 const App = () => {
+  const [cores, setCores] = React.useState([]);
+
+  function handleChange({ target }) {
+    const { checked, value } = target;
+    if (checked) {
+      setCores([...cores, target.value]);
+    } else {
+      setCores(cores.filter((cor) => cor !== value));
+    }
+  }
+
+  function handleChecked(cor) {
+    return cores.includes(cor);
+  }
+
   return (
-    <div>
-      <Input />
-    </div>
+    <form>
+      {coresArray.map((cor) => (
+        <label key={cor} style={{ textTransform: "capitalize" }}>
+          <input
+            type="checkbox"
+            value={cor}
+            checked={handleChecked(cor)}
+            onChange={handleChange}
+          />
+          {cor}
+        </label>
+      ))}
+      <ul>
+        {cores.map((cor) => (
+          <li key={cor}>{cor}</li>
+        ))}
+      </ul>
+    </form>
   );
 };
 
