037d3911dcee0dc217443a8b8acb1fcb46d80619
Igor Bitencourt <bitencourt.igor@hotmail.com>
2022-07-23 11:26:30 -0300

    preparacao para exercicio rotas

--- estudos/src/App.js
+++ estudos/src/App.js
@@ -1,27 +1,7 @@
 import React from "react";
-import Slide from "./Slide";
 
 const App = () => {
-  const slides = [
-    {
-      id: "slide1",
-      text: "Slide 1",
-    },
-    {
-      id: "slide2",
-      text: "Slide 2",
-    },
-    {
-      id: "slide3",
-      text: "Slide 3",
-    },
-  ];
-
-  return (
-    <div>
-      <Slide slides={slides} />
-    </div>
-  );
+  return <div></div>;
 };
 
 export default App;
